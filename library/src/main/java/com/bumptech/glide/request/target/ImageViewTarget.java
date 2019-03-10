21186001855f8d81c08aa3630e3c98d8a65b23d7
ZhangKe <zhangke_711@163.com>
2019-03-10 23:59:48 +0800

    格式化代码

--- library/src/main/java/com/bumptech/glide/request/target/ImageViewTarget.java
+++ library/src/main/java/com/bumptech/glide/request/target/ImageViewTarget.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.widget.ImageView;
+
 import com.bumptech.glide.request.transition.Transition;
 
 /**
@@ -17,126 +18,126 @@ import com.bumptech.glide.request.transition.Transition;
 // Public API.
 @SuppressWarnings("WeakerAccess")
 public abstract class ImageViewTarget<Z> extends ViewTarget<ImageView, Z>
-    implements Transition.ViewAdapter {
+        implements Transition.ViewAdapter {
 
-  @Nullable
-  private Animatable animatable;
+    @Nullable
+    private Animatable animatable;
 
-  public ImageViewTarget(ImageView view) {
-    super(view);
-  }
-
-  /**
-   * @deprecated Use {@link #waitForLayout()} instead.
-   */
-  @SuppressWarnings({"deprecation"})
-  @Deprecated
-  public ImageViewTarget(ImageView view, boolean waitForLayout) {
-    super(view, waitForLayout);
-  }
-
-  /**
-   * Returns the current {@link android.graphics.drawable.Drawable} being displayed in the view
-   * using {@link android.widget.ImageView#getDrawable()}.
-   */
-  @Override
-  @Nullable
-  public Drawable getCurrentDrawable() {
-    return view.getDrawable();
-  }
-
-  /**
-   * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
-   * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
-   *
-   * @param drawable {@inheritDoc}
-   */
-  @Override
-  public void setDrawable(Drawable drawable) {
-    view.setImageDrawable(drawable);
-  }
-
-  /**
-   * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
-   * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
-   *
-   * @param placeholder {@inheritDoc}
-   */
-  @Override
-  public void onLoadStarted(@Nullable Drawable placeholder) {
-    super.onLoadStarted(placeholder);
-    setResourceInternal(null);
-    setDrawable(placeholder);
-  }
-
-  /**
-   * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
-   * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
-   *
-   * @param errorDrawable {@inheritDoc}
-   */
-  @Override
-  public void onLoadFailed(@Nullable Drawable errorDrawable) {
-    super.onLoadFailed(errorDrawable);
-    setResourceInternal(null);
-    setDrawable(errorDrawable);
-  }
-
-  /**
-   * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
-   * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
-   *
-   * @param placeholder {@inheritDoc}
-   */
-  @Override
-  public void onLoadCleared(@Nullable Drawable placeholder) {
-    super.onLoadCleared(placeholder);
-    if (animatable != null) {
-      animatable.stop();
+    public ImageViewTarget(ImageView view) {
+        super(view);
     }
-    setResourceInternal(null);
-    setDrawable(placeholder);
-  }
 
-  @Override
-  public void onResourceReady(@NonNull Z resource, @Nullable Transition<? super Z> transition) {
-    if (transition == null || !transition.transition(resource, this)) {
-      setResourceInternal(resource);
-    } else {
-      maybeUpdateAnimatable(resource);
+    /**
+     * @deprecated Use {@link #waitForLayout()} instead.
+     */
+    @SuppressWarnings({"deprecation"})
+    @Deprecated
+    public ImageViewTarget(ImageView view, boolean waitForLayout) {
+        super(view, waitForLayout);
     }
-  }
 
-  @Override
-  public void onStart() {
-    if (animatable != null) {
-      animatable.start();
+    /**
+     * Returns the current {@link android.graphics.drawable.Drawable} being displayed in the view
+     * using {@link android.widget.ImageView#getDrawable()}.
+     */
+    @Override
+    @Nullable
+    public Drawable getCurrentDrawable() {
+        return view.getDrawable();
     }
-  }
 
-  @Override
-  public void onStop() {
-    if (animatable != null) {
-      animatable.stop();
+    /**
+     * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
+     * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
+     *
+     * @param drawable {@inheritDoc}
+     */
+    @Override
+    public void setDrawable(Drawable drawable) {
+        view.setImageDrawable(drawable);
     }
-  }
 
-  private void setResourceInternal(@Nullable Z resource) {
-    // Order matters here. Set the resource first to make sure that the Drawable has a valid and
-    // non-null Callback before starting it.
-    setResource(resource);
-    maybeUpdateAnimatable(resource);
-  }
-
-  private void maybeUpdateAnimatable(@Nullable Z resource) {
-    if (resource instanceof Animatable) {
-      animatable = (Animatable) resource;
-      animatable.start();
-    } else {
-      animatable = null;
+    /**
+     * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
+     * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
+     *
+     * @param placeholder {@inheritDoc}
+     */
+    @Override
+    public void onLoadStarted(@Nullable Drawable placeholder) {
+        super.onLoadStarted(placeholder);
+        setResourceInternal(null);
+        setDrawable(placeholder);
     }
-  }
 
-  protected abstract void setResource(@Nullable Z resource);
+    /**
+     * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
+     * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
+     *
+     * @param errorDrawable {@inheritDoc}
+     */
+    @Override
+    public void onLoadFailed(@Nullable Drawable errorDrawable) {
+        super.onLoadFailed(errorDrawable);
+        setResourceInternal(null);
+        setDrawable(errorDrawable);
+    }
+
+    /**
+     * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
+     * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
+     *
+     * @param placeholder {@inheritDoc}
+     */
+    @Override
+    public void onLoadCleared(@Nullable Drawable placeholder) {
+        super.onLoadCleared(placeholder);
+        if (animatable != null) {
+            animatable.stop();
+        }
+        setResourceInternal(null);
+        setDrawable(placeholder);
+    }
+
+    @Override
+    public void onResourceReady(@NonNull Z resource, @Nullable Transition<? super Z> transition) {
+        if (transition == null || !transition.transition(resource, this)) {
+            setResourceInternal(resource);
+        } else {
+            maybeUpdateAnimatable(resource);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        if (animatable != null) {
+            animatable.start();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (animatable != null) {
+            animatable.stop();
+        }
+    }
+
+    private void setResourceInternal(@Nullable Z resource) {
+        // Order matters here. Set the resource first to make sure that the Drawable has a valid and
+        // non-null Callback before starting it.
+        setResource(resource);
+        maybeUpdateAnimatable(resource);
+    }
+
+    private void maybeUpdateAnimatable(@Nullable Z resource) {
+        if (resource instanceof Animatable) {
+            animatable = (Animatable) resource;
+            animatable.start();
+        } else {
+            animatable = null;
+        }
+    }
+
+    protected abstract void setResource(@Nullable Z resource);
 }
 
